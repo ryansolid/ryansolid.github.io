0d901661d30af2009705575fff46975a96c9a676
Ryan Carniato <ryansolid@gmail.com>
2019-05-12 19:50:40 -0700

    Add Solid Hacker News clone

---
+++ solid-hackernews-app/precache-manifest.66522da210f97b6f01acf2b995bb4ceb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00340aad7a54ea5c02757f2a385e168c",
+    "url": "/solid-hackernews-app/index.html"
+  },
+  {
+    "revision": "744f76fef59f5e2b61a2",
+    "url": "/solid-hackernews-app/static/js/0.f176da25.chunk.js"
+  },
+  {
+    "revision": "d941254dae1d8c16fef3",
+    "url": "/solid-hackernews-app/static/js/10.0fe359a7.chunk.js"
+  },
+  {
+    "revision": "3312ff3d468efeb7ce10",
+    "url": "/solid-hackernews-app/static/js/11.9657b517.chunk.js"
+  },
+  {
+    "revision": "09e76159e64b82fd218b",
+    "url": "/solid-hackernews-app/static/js/app-nav.fbbcb5ba.chunk.js"
+  },
+  {
+    "revision": "d98120b1836b57cece8a",
+    "url": "/solid-hackernews-app/static/js/comment-item.f13545fd.chunk.js"
+  },
+  {
+    "revision": "dff6d7950f0ffb09ec0e",
+    "url": "/solid-hackernews-app/static/js/hacker-news.8751e68d.chunk.js"
+  },
+  {
+    "revision": "c308fa12b9999f7c43e2",
+    "url": "/solid-hackernews-app/static/js/main.dafd5980.chunk.js"
+  },
+  {
+    "revision": "ca7560b03f41553b4a4c",
+    "url": "/solid-hackernews-app/static/js/runtime~main.22520553.js"
+  },
+  {
+    "revision": "1bc4158457b9b16de5b4",
+    "url": "/solid-hackernews-app/static/js/stories-page.35ac1e2a.chunk.js"
+  },
+  {
+    "revision": "41e735a3fa9a68871001",
+    "url": "/solid-hackernews-app/static/js/story-item.db0fe82a.chunk.js"
+  },
+  {
+    "revision": "d7be8fea616def0ac526",
+    "url": "/solid-hackernews-app/static/js/story-page.da775d7c.chunk.js"
+  },
+  {
+    "revision": "b7e68bc30f2402fc911c",
+    "url": "/solid-hackernews-app/static/js/user-page.8f021b48.chunk.js"
+  }
+]);
